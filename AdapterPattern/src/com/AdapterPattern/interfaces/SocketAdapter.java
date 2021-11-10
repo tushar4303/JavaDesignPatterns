ee036b0e53beb04ba5b0c191cc1750a709b871be
Tushar Padhy <88235295+tushar4303@users.noreply.github.com>
2021-11-10 23:35:37 +0530

    add Adapterpattern design package

---
+++ AdapterPattern/src/com/AdapterPattern/interfaces/SocketAdapter.java
@@ -0,0 +1,14 @@
+package com.AdapterPattern.interfaces;
+
+import com.AdapterPattern.model.Volt;
+
+public interface SocketAdapter {
+
+    Volt get120Volts();
+
+    Volt get12Volts();
+
+    Volt get3Volts();
+
+    Volt get1Volt();
+}
---
+++ AdapterPattern/src/com/AdapterPattern/model/Volt.java
@@ -0,0 +1,18 @@
+package com.AdapterPattern.model;
+
+public class Volt {
+    private int volts;
+
+
+    public Volt(int volts) {
+        this.volts = volts;
+    }
+
+    public int getVolts() {
+        return volts;
+    }
+
+    public void setVolts(int volts) {
+        this.volts = volts;
+    }
+}
