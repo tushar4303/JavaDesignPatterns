c55f6a39e50a87e8f84fc495bcc0d09461866729
Tushar Padhy <88235295+tushar4303@users.noreply.github.com>
2021-11-10 23:56:24 +0530

    pushing templatemethodpattern to repo

---
+++ TemplateMethodPattern/src/com/TemplateMethodPattern/Main.java
@@ -0,0 +1,12 @@
+package com.TemplateMethodPattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Game game = new EndlessRunnerGame();
+        game.play();
+
+
+    }
+}
